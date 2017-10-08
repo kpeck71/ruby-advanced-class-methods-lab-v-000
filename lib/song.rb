d3aa07591c7c8872f385e5c0bb892080b06d764f
Kristen Peck <kpeck71@gmail.com>
2017-10-08 13:13:12 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -42,9 +42,6 @@ class Song
     find_by_name(name) || create_by_name(name)
   end
 
-
-
-
   def Song.alphabetical
     @@songs.sort_by{|song| song.name}
   end
@@ -61,13 +58,14 @@ class Song
   end
 
   def Song.create_from_filename(filename)
-    file = filename.split(" - ") #["Thundercat ", " For Love I Come.mp3"]
-    artist_name = file[0]
-    name = file[1].chomp(".mp3")
-    song2 = Song.new
-    song2.name = name
-    song2.artist_name = artist_name
-    song2.save
+    new_from_filename.save
+    # file = filename.split(" - ") #["Thundercat ", " For Love I Come.mp3"]
+    # artist_name = file[0]
+    # name = file[1].chomp(".mp3")
+    # song2 = Song.new
+    # song2.name = name
+    # song2.artist_name = artist_name
+    # song2.save
   end
 
   def self.destroy_all
